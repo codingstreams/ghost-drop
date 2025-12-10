c206c4367240799ae77871c9d7928d8facefc556
akxhdev <akxhdev@gmail.com>
2025-12-10 20:33:01 +0530

    Init Commit

---
+++ src/main/java/in/codingstreams/ghost_drop/GhostDropApplication.java
@@ -0,0 +1,13 @@
+package in.codingstreams.ghost_drop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostDropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/codingstreams/ghost_drop/GhostDropApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codingstreams.ghost_drop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
