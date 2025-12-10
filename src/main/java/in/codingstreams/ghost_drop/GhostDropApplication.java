4e0d94c53c360262e26ef02fd774149eeee4d88b
akxhdev <akxhdev@gmail.com>
2025-12-10 22:10:19 +0530

    feat: configure PostgreSQL database and verify connection

--- src/main/java/in/codingstreams/ghost_drop/GhostDropApplication.java
+++ src/main/java/in/codingstreams/ghost_drop/GhostDropApplication.java
@@ -1,7 +1,10 @@
 package in.codingstreams.ghost_drop;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class GhostDropApplication {
